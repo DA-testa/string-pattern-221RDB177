3455d7fe14ef8d5bc82ddb1a0df26c2f6e984b15
221RDB177 <125874315+221RDB177@users.noreply.github.com>
2023-04-16 20:41:24 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,59 +1,32 @@
-# python3
+def read_input(): input_type = input() # Check for input type - "I" or "F"
 
-def read_input():
-    # this function acquires input from both keyboard and file
-    # use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    input_type = input().rstrip().lower()
-    if input_type == 'i':
-        # read input from keyboard
-        pattern = input().rstrip()
-        text = input().rstrip()
-    elif input_type == 'f':
-        # read input from file
-        filename = input().rstrip()
-        with open(filename, 'r') as f:
-            pattern = f.readline().rstrip()
-            text = f.readline().rstrip()
-    else:
-        raise ValueError("Invalid input type specified. Must be 'i' or 'f'.")
-    
-    # return both lines in one return
-    return pattern, text
+if "I" in input_type: # If input type is "I"
+    first_pattern = input().strip() # Input the first line which contains pattern
+    second_text = input().strip() # Input the second line which contains text
+    return (first_pattern, second_text) # Return both lines
 
-def print_occurrences(output):
-    # this function controls output, it doesn't need any return
-    print(' '.join(map(str, output)))
+if "F" in input_type: # If input type is "F"
+    with open("tests/06") as file: # Open the file "tests/06" and save as file
+        first_line = file.readline().strip()   # Read the first line which contains pattern
+        second_line = file.readline().strip()  # Read the second line which contains text
+    return (first_line, second_line) # Return both lines
+def print_occurrences(output): # This function controls the output, it doesn't need to return anything print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    # this function finds the occurrences using Rabin-Karp algorithm
-    
-    # calculate the hash of the pattern
-    pattern_hash = sum(ord(pattern[i]) * pow(26, len(pattern)-i-1) for i in range(len(pattern)))
-    
-    # initialize variables
-    text_hash = 0
-    output = []
-    prime = 101
-    
-    # calculate the hash of the first substring of the text of the same length as the pattern
-    for i in range(len(pattern)):
-        text_hash += ord(text[i]) * pow(26, len(pattern)-i-1)
-    
-    # check if the hash of the first substring of text matches the hash of the pattern
-    if text_hash == pattern_hash and text[:len(pattern)] == pattern:
-        output.append(0)
-    
-    # calculate the hash of each subsequent substring of text and check if it matches the hash of the pattern
-    for i in range(1, len(text)-len(pattern)+1):
-        text_hash = (text_hash - ord(text[i-1]) * pow(26, len(pattern)-1)) * 26 + ord(text[i+len(pattern)-1])
-        if text_hash == pattern_hash and text[i:i+len(pattern)] == pattern:
-            output.append(i)
-    
-    # return the list of occurrences
-    return output
+def get_occurrences(pattern, text): # This function finds the occurrences using Rabin-Karp algorithm p = len(pattern) t = len(text)
 
+occurrences = []
 
-# this part launches the functions
-if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+ph = hash(pattern) # Calculate the hash of the pattern
+th = hash(text[:p]) # Calculate the hash of the first substring of the text
+
+for i in range(t-p+1):
+    if ph == th and text[i:i+p] == pattern: # If the hashes match and the substring equals the pattern
+        occurrences.append(i) # Add the index of the substring to the list of occurrences
+
+    if i < t-p: # If there are more substrings to check
+        th = hash(text[i+1:i+p+1]) # Calculate the hash of the next substring
+
+# Return an iterable variable
+return occurrences
+
+if name == 'main': print_occurrences(get_occurrences(*read_input()))
